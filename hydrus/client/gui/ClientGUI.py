4f0020e16fa2c3a4ee920374645c8185808542c9
hydev <hydrus.admin@gmail.com>
2026-07-21 19:31:32 -0500

    Removing old safety check on systray in non-advanced Linux

--- hydrus/client/gui/ClientGUI.py
+++ hydrus/client/gui/ClientGUI.py
@@ -7360,7 +7360,7 @@ The password is cleartext here but obscured in the entry dialog. Enter a blank p
         
     def _UpdateSystemTrayIcon( self, currently_booting = False ):
         
-        if not ClientGUISystemTray.SystemTrayAvailable() or ( not (HC.PLATFORM_WINDOWS or HC.PLATFORM_MACOS ) and not CG.client_controller.new_options.GetBoolean( 'advanced_mode' ) ):
+        if not ClientGUISystemTray.SystemTrayAvailable():
             
             return
             
